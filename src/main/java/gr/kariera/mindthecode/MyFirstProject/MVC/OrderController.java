116a9cb1014d7abc056729829174b38cd08acc2f
Thanassis Karamanos <thanassis1988@hotmail.gr>
2023-03-18 13:33:35 +0200

    product service product mvc controller

--- src/main/java/gr/kariera/mindthecode/MyFirstProject/MVC/OrderController.java
+++ src/main/java/gr/kariera/mindthecode/MyFirstProject/MVC/OrderController.java
@@ -1,4 +1,16 @@
 package gr.kariera.mindthecode.MyFirstProject.MVC;
 
+import gr.kariera.mindthecode.MyFirstProject.Repositories.OrderRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/orders")
 public class OrderController {
+
+    private final OrderRepository repo;
+    private final ProductRepository productRepo;
+
+
+
 }
--- src/main/java/gr/kariera/mindthecode/MyFirstProject/Services/ProductService.java
+++ src/main/java/gr/kariera/mindthecode/MyFirstProject/Services/ProductService.java
@@ -1,4 +1,19 @@
 package gr.kariera.mindthecode.MyFirstProject.Services;
 
+
+import gr.kariera.mindthecode.MyFirstProject.Entities.Product;
+import org.springframework.data.domain.Page;
+
 public interface ProductService {
+
+    public abstract Product createOrUpdateProduct(Integer id, Product product) throws Exception;
+    public abstract Product getById(Integer id);
+    public abstract void deleteProduct(Integer id);
+    public abstract Page<Product> getProducts(
+            String description,
+            int page,
+            int size,
+            String sort
+    );
+
 }
